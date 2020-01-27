4eeb05aadbbcf3cebc6877d7d224d8e1d5227d55
rileybeckham <42828365+rileybeckham@users.noreply.github.com>
2020-01-27 13:24:23 -0700

    Update script.js

--- Work/HW2/script.js
+++ Work/HW2/script.js
@@ -1,6 +1,7 @@
-// JavaScript source code
 /*Author: Riley Beckham
- File Name: 
+ File Name: HW2
+ Date:1/27/2020
+ purpose: HW2 for 411
  */
 
 console.log("My 3 favorite websites are: Stack Overflow, reddit, and w3schools.");
