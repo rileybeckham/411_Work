5151ff763045eb3cf37a206268133323a71b8956
rileybeckham <42828365+rileybeckham@users.noreply.github.com>
2020-01-27 13:12:29 -0700

    Update script.js

--- Work/HW2/script.js
+++ Work/HW2/script.js
@@ -5,4 +5,4 @@
 
 console.log("My 3 favorite websites are: Stack Overflow, reddit, and w3schools.");
 console.log("My 3 favorite games currently(not all time): Ghost Recon BreakPoint, NBA2k20, and Battlefront II.");
-console.log("My 3 favorite artists currently are: Mike Perry, Rodrigo Corral, and Yoshinori Kondo")
+console.log("My 3 favorite artists currently are: Mike Perry, Rodrigo Corral, and Yoshinori Kondo.")
