899aa05ff319d46302f2051db12f29e234424299
Riley Beckham <riley.beckham@umconnect.umt.edu>
2020-01-27 13:00:03 -0700

    HW2

---
+++ Work/HW2/script.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+/*Author: Riley Beckham
+ File Name: 
+ */
+
+console.log("My 3 favorite websites are: Stack Overflow, reddit, and w3schools.");
+console.log("My 3 favorite games currently(not all time): Ghost Recon BreakPoint, NBA2k20, and Battlefront II.");
+console.log("My 3 favorite artists currently are: Mike Perry, Rodrigo Corral, and Yoshinori Kondo")
