274f9986f7a361b3cba24869ff3666f13f9e8729
Riley Beckham <riley.beckham@umconnect.umt.edu>
2020-04-16 23:23:18 -0600

    fixed broken code, addded readme

--- Work/HW11/scripts/canvas.js
+++ Work/HW11/scripts/canvas.js
@@ -8,88 +8,86 @@ var questions;
 var squareArray = [];
 var circArray = [];
 var score = 0;
-$(document).ready(function(){
-    
-    setup();  
-    
-    $(this).keypress(function(event){
+$(document).ready(function() {
+
+    setup();
+
+    $(this).keypress(function(event) {
         getKey(event);
-        
+
     });
 });
 
 
 
-function setup()
-{
+function setup() {
     canvas = document.getElementById("myCanvas");
     ctx = canvas.getContext("2d");
 
     // create two objects
     square1 = new Square(100, 100, 50, 50, "#0000FF");
 
-    circ1 = new Circle(75, 75, 45, "#0000FF");
-   // square2 = new Square(400,400,100,100,"#00FF00");
+    /////////
+    //circ1 = new Circle(75, 75, 45, "#0000FF");
+    ////////////
+
+    // square2 = new Square(400,400,100,100,"#00FF00");
     $.getJSON("data/squares.json", function(data) {
-        for(var i = 0; i < data.squares.length; i++)
-        {
+
+        for (var i = 0; i < data.squares.length; i++) {
             squareArray.push(new Square(data.squares[i].x, data.squares[i].y, data.squares[i].h, data.squares[i].w, data.squares[i].color));
-            //circArray.push(new Circle(data.circles[i].x, data.circles[i].y, data.circles[i].r, data.cirlces[i].color));
+
+            // there was a mispelling here with the last circles
+            //circArray.push(new Circle(data.circles[i].x, data.circles[i].y, data.circles[i].r, data.circles[i].color));
         }
         draw();
     });
-    //$.getJSON("data/circles.json", function(data) {
-    //    for (var i = 0; i < data.circles.length; i++) {
-    //        circArray.push(new Circle(data.circles[i].x, data.circles[i].y, data.circles[i].r, data.cirlces[i].color));
-    //    }
-    //    draw();
-    //});
+     $.getJSON("data/circles.json", function(data) {
+         console.log("HI " + data.circles.length);
+         for (var i = 0; i < data.circles.length; i++) {
+             circArray.push(new Circle(data.circles[i].x, data.circles[i].y, data.circles[i].r, data.circles[i].color));
+         }
+         draw();
+     });
 
-    
 
-    
+
+
 }
 
-function getKey(event)
-{
+function getKey(event) {
     var delsquare;
     var delcirc;
     var char = event.which || event.keyCode;
     var actualLetter = String.fromCharCode(char);
-    if(actualLetter == "w")
-    {
+    if (actualLetter == "w") {
         moveUp();
         direction = "up";
     }
-    if(actualLetter == "s")
-    {
+    if (actualLetter == "s") {
         moveDown();
         direction = "down";
     }
-    if(actualLetter == "a")
-    {
+    if (actualLetter == "a") {
         moveLeft();
         direction = "left";
     }
-    if(actualLetter == "d")
-    {
+    if (actualLetter == "d") {
         moveRight();
         direction = "right";
     }
     //var test = hasCollided(square1,square2);
     var test2 = false;
     var test3 = false;
-    for(var i = 0; i < squareArray.length; i++)
-    {
+    for (var i = 0; i < squareArray.length; i++) {
 
-        test2 = hasCollided(square1,squareArray[i]);
-        if(test2 == true)
-        {
+        test2 = hasCollided(square1, squareArray[i]);
+        if (test2 == true) {
             delsquare = i;
             break;
         }
 
-    
+
         //console.log(test2);
     }
     for (var i = 0; i < circArray.length; i++) {
@@ -100,71 +98,62 @@ function getKey(event)
             break;
         }
     }
-        if (test2) {
+    if (test2) {
 
-            score++;
-            squareArray.splice(delsquare, 1);
-            if (direction == "left") {
-                moveRight();
-            }
-            else if (direction == "right") {
-                moveLeft();
-            }
-            else if (direction == "up") {
-                moveDown();
-            }
-            else if (direction == "down") {
-                moveUp();
-            }
+        score++;
+        squareArray.splice(delsquare, 1);
+        if (direction == "left") {
+            moveRight();
+        } else if (direction == "right") {
+            moveLeft();
+        } else if (direction == "up") {
+            moveDown();
+        } else if (direction == "down") {
+            moveUp();
         }
-        if (test3) {
-
-            score++;
-            circArray.splice(delcirc, 1);
-            if (direction == "left") {
-                moveRight();
-            }
-            else if (direction == "right") {
-                moveLeft();
-            }
-            else if (direction == "up") {
-                moveDown();
-            }
-            else if (direction == "down") {
-                moveUp();
-            }
     }
-    draw(); 
-    
+    if (test3) {
+
+        score++;
+        circArray.splice(delcirc, 1);
+        if (direction == "left") {
+            moveRight();
+        } else if (direction == "right") {
+            moveLeft();
+        } else if (direction == "up") {
+            moveDown();
+        } else if (direction == "down") {
+            moveUp();
+        }
+    }
+    draw();
+
 }
 
-function moveUp()
-{
-    square1.y-=10;
-}
-function moveDown()
-{
-    square1.y+=10;
-}
-function moveRight()
-{
-    square1.x+=10;
-}
-function moveLeft()
-{
-    square1.x-=10;
+function moveUp() {
+    square1.y -= 10;
 }
 
-function draw()
-{
-    ctx.clearRect(0,0,800,600);
+function moveDown() {
+    square1.y += 10;
+}
+
+function moveRight() {
+    square1.x += 10;
+}
+
+function moveLeft() {
+    square1.x -= 10;
+}
+
+function draw() {
+    ctx.clearRect(0, 0, 800, 600);
     ctx.fillStyle = square1.mainColor;
     ctx.fillRect(square1.x, square1.y, square1.width, square1.height);
-    
-    ctx.fillStyle = square2.mainColor;
-    ctx.fillRect(square2.x, square2.y, square2.width, square2.height);
-    for(var i = 0; i < squareArray.length; i++)
-    {
+
+    // ctx.fillStyle = square2.mainColor;
+    //ctx.fillRect(square2.x, square2.y, square2.width, square2.height);
+    for (var i = 0; i < squareArray.length; i++) {
         ctx.fillStyle = squareArray[i].mainColor;
         ctx.fillRect(squareArray[i].x, squareArray[i].y, squareArray[i].width, squareArray[i].height);
     }
@@ -176,7 +165,7 @@ function draw()
     }
 
     ctx.font = "30px Arial";
-    ctx.fillText("Score: " + score, 10, 50);    
+    ctx.fillText("Score: " + score, 10, 50);
 
 }
 
@@ -190,8 +179,8 @@ function hasCollided(object1, object2) {
     );
 }
 
-
-class Circle {
+// no need for this class since you had it in another file that was included
+/*class Circle {
     constructor(xCord, yCord, radius, color) {
         this.xCord = xCord;
         this.yCord = yCord;
@@ -233,4 +222,4 @@ class Circle {
     get color() {
         return this.color;
     }
-}
+}*/
--- Work/HW11/scripts/circle.js
+++ Work/HW11/scripts/circle.js
@@ -1,10 +1,10 @@
-class Circle{
+class Circle {
     constructor(xCord, yCord, radius, color) {
         this.xCord = xCord;
         this.yCord = yCord;
         this.radius = radius;
-        this.startPoint = 0;
-        this.endPoint = 2 * Math.PI;
+        this.startpoint = 0;
+        this.endpoint = 2 * Math.PI;
         this.color = color;
     }
     get x() {
@@ -23,21 +23,27 @@ class Circle{
         return this.radius;
     }
     set r(r) {
-        this.radius = r;
-    }
+            this.radius = r;
+        }
+        // had the same capitalization for startPoint.  
+        //That caused an issue. Needed make the variable lower case
     get startPoint() {
-        return this.startPoint;
+        return this.startpoint;
     }
-    set startPoint(startPoint) {
-        this.startPoint = startPoint;
+    set startPoint(startpoint) {
+        this.startpoint = startpoint;
     }
     get endPoint() {
-        return this.endPoint;
+        return this.endpoint;
     }
-    set endPoint(endPoint) {
-        this.endPoint = endPoint;
+    set endPoint(endpoint) {
+        this.endpoint = endpoint;
     }
-    get color() {
+    get Color() {
         return this.color;
+    }
+
+    set Color(color) {
+        this.color = color;
     }
 }
